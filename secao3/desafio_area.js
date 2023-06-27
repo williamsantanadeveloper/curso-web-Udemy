ddbf944a31c84849216a9f40cfbc69554ec23ab2
William Santana <williamandrade1058@gmail.com>
2023-06-27 16:28:59 -0300

    Create desafio_area.js

---
+++ secao3/desafio_area.js
@@ -0,0 +1,10 @@
+// area = PI * r**2
+
+let raio = 10;
+const PI = 3.141592;
+let area = PI * (raio ** 2); // ou Math.PI * (raio ** 2)
+console.log("A área da circuferência de é de: " + area + " metros^2")
+
+area = Math.PI * (raio ** 2);
+
+console.log("A área da circuferência de é de: " + area + " metros^2")
