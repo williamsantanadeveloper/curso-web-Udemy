64a422667ba48079f4ca115e515f0e57a8435c13
William Santana <williamandrade1058@gmail.com>
2023-06-27 10:56:43 -0300

    Create desafio_setenca_bloco.js

---
+++ secao3/desafio_setenca_bloco.js
@@ -0,0 +1,27 @@
+/* 
+Exibir números ímpares(entre 1 e 10)
+Exibir números pares(entre 1 e 10)
+Em blocos diferentes
+*/
+
+// Bloco ímpares
+{
+    console.log("Números ímpares")
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+console.log("-------------------------------------------------------")
+// Bloco pares
+
+{
+    console.log("Números pares")
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
