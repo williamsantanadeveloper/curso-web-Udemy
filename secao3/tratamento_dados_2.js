dc7671b2049bfda48ca5f289104468b9fbbc39f2
William Santana <williamandrade1058@gmail.com>
2023-06-27 15:58:45 -0300

    Create tratamento_dados_2.js

---
+++ secao3/tratamento_dados_2.js
@@ -0,0 +1,12 @@
+let preco = 19.90;
+let desconto = 0.40;
+
+console.log(preco * (1 - desconto));
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno"; // -> String é uma sequência de caracteres
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome + ", Categoria: " + categoria + ", Preço: " + preco + ", Desconto: " + desconto);
