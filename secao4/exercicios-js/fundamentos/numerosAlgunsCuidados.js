c1ffe9338fdf9afb8ffe6df8fbbd18631309bad4
William Santana <williamandrade1058@gmail.com>
2023-06-28 17:49:47 -0300

    Update numerosAlgunsCuidados.js

--- secao4/exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ secao4/exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,1 +1,7 @@
-c
+console.log(7 / 0);
+console.log("10.2" / 2);
+console.log("Show!" * 2);
+console.log(0.1 + 0.7);
+//console.log(10.0.toString(2));
+//console.log(10.toString());
+console.log((10.345).toFixed(2))
