00aa68ac3292fab17dc376453d4ff2129b1a7656
William Santana <williamandrade1058@gmail.com>
2023-06-28 17:31:38 -0300

    Update tipagemFraca.js

---
+++ secao4/exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = "legal";
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes genéricos e siglas
+
+let valor = "";
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkkk
+
+console.log(typeof pqp)
