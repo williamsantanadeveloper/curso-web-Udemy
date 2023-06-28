c60afd691f04ba1e4468776d9d527c5ca95a8140
William Santana <williamandrade1058@gmail.com>
2023-06-28 09:26:10 -0300

    Create organizacao.js

---
+++ secao4/exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código");
+
+{
+   {
+        console.log("Olá");
+        console.log("Mundo!");
+   } 
+}
+
+// JavaScript é formado por sentenças de códigos podendo terminar ou não com ; e também é formado por blocos com as chaves {}
