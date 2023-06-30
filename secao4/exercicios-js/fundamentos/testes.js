b6d258ac1d528876ab050372af8683f129507bd1
William Santana <williamandrade1058@gmail.com>
2023-06-30 18:11:44 -0300

    Update testes.js

--- secao4/exercicios-js/fundamentos/testes.js
+++ secao4/exercicios-js/fundamentos/testes.js
@@ -1,11 +1,11 @@
-let nomesArray = ['William', "Coder"];
-console.log(nomesArray)
-console.log(typeof nomesArray)
-console.log(nomesArray.replace(",", "/"))
-let a = 1;
-let b = 2;
-let temporario = a;
-a = b;
-b = temporario;
-console.log(b)
-console.log(a)
+let nome = {};
+nome.escola = "IDFG";
+nome.Aluno = "William";
+nome.PrecoUniforme = 90
+
+console.log(nome)
+
+const Nome = {
+    
+
+}
