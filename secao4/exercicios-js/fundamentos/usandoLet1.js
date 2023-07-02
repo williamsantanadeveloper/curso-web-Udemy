b6028fd892bd9b6b3608f1c457d32ec43800265e
William Santana <williamandrade1058@gmail.com>
2023-07-01 23:24:35 -0300

    Update usandoLet1.js

---
+++ secao4/exercicios-js/fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+let numero = 1
+{
+    let numero = 2
+    console.log("dentro = ".concat(numero))
+}
+console.log("fora = ".concat(numero))
+
+// Var tem escopo global, função
+// Let tem escopo global, função e bloco
