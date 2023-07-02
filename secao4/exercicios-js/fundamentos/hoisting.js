647ec3bc4d18970917caf6b2de23378a50745aac
William Santana <71521910+williamsantanadeveloper@users.noreply.github.com>
2023-07-02 11:05:53 -0300

    Update hoisting.js

--- secao4/exercicios-js/fundamentos/hoisting.js
+++ secao4/exercicios-js/fundamentos/hoisting.js
@@ -1,4 +1,4 @@
-
+// Hoisting (içamento, em português) é um termo que você não encontrará usado em nenhuma prosa de especificação normativa antes da especificação de idioma do ECMAScript® 2015.
 console.log('a =', a);
 var a = 2;
 console.log("a = ", a);
