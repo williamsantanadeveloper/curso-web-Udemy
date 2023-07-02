e915092c9449be10831172e3c31de7c062eeb6a0
William Santana <williamandrade1058@gmail.com>
2023-07-01 22:59:11 -0300

    Update testes.js

--- secao4/exercicios-js/fundamentos/testes.js
+++ secao4/exercicios-js/fundamentos/testes.js
@@ -1,7 +1,25 @@
-let valor = [2, 5, "Vasco", 7];
+// let valor = [2, 5, "Vasco", 7];
 
-valor.push(1);
-console.log(valor)
-valor.pop()
+// valor.push(1);
+// console.log(valor)
+// valor.pop()
 
-console.log(valor)
+// console.log(valor)
+
+function divisao(a, b) {
+    console.log(a / b)
+}
+divisao(2, 4)
+
+function div (c, d) {
+    return c / d;
+}
+console.log(div(2, 3))
+
+const outraDivisao = function (f, g) {
+    console.log(f / g)
+}
+outraDivisao(10, 4)
+
+const outraDivisao2 = (m,n) => m / n
+console.log(outraDivisao2(10, 10))
