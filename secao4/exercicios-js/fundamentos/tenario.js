350abbd8aae511e2c7f9eb1cf037b42ba4490ad1
William Santana <williamandrade1058@gmail.com>
2023-07-05 16:35:27 -0300

    update ternario

---
+++ secao4/exercicios-js/fundamentos/tenario.js
@@ -0,0 +1,16 @@
+const resultado = nota => nota >= 7 ? "Aprovado": "Reprovado" // 1º estrutura relacional nota >= 7 / 2º Se for verdadeiro retorna verdadeiro "Aprovado" / 3º Se for falso retorna "Reprovado"
+// Estrutura nesse exemplo: nota >= 7 ? "Aprovado": "Reprovado"
+
+console.log(resultado(7.1));
+console.log(resultado(6.7));
+
+// outra maneira
+
+/* 
+const resultado = nota {
+    return >= 7 ? "Aprovado": "Reprovado"
+}
+
+console.log(resultado(7.1));
+console.log(resultado(6.7));
+*/
