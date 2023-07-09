742762b78c7773e9aa5459ef02bb46c55cd444dd
William Santana <williamandrade1058@gmail.com>
2023-07-09 12:17:45 -0300

    update while and testes

--- secao5/estruturas_controle/testes.js
+++ secao5/estruturas_controle/testes.js
@@ -1,21 +1,36 @@
-Number.prototype.entre = function(inicio, fim) {
-    return this >= inicio && this <= fim
-}
+// Number.prototype.entre = function(inicio, fim) {
+//     return this >= inicio && this <= fim
+// }
 
-function boletim(media) {
-    if (media.entre(6, 10)) {
-        console.log("Aprovado com média " .concat(media))
-    }
-    else if (media.entre(5, 5.9)) {
-        console.log("Recuperação ".concat(media))
-    }
-    else if (media.entre(0, 4.9)){
-        console.log("Reprovado ".concat(media))
-    }
-    else {
-        console.log("Não conheço essa nota")
+// function boletim(media) {
+//     if (media.entre(6, 10)) {
+//         console.log("Aprovado com média " .concat(media))
+//     }
+//     else if (media.entre(5, 5.9)) {
+//         console.log("Recuperação ".concat(media))
+//     }
+//     else if (media.entre(0, 4.9)){
+//         console.log("Reprovado ".concat(media))
+//     }
+//     else {
+//         console.log("Não conheço essa nota")
+//     }
+// }
+// boletim(6)
+// boletim(5.2)
+// boletim(4)
+
+
+
+
+
+
+const pessoa = {
+    nome: "William",
+    idade: 18,
+    endereco:  {
+        rua: "Avenida",
+        numero: 1350
     }
 }
-boletim(6)
-boletim(5.2)
-boletim(4)
+console.log(pessoa.endereco.rua)
