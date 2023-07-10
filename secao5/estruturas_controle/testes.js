5f0b3dcc792d8b23a72392e16d2ca5c7cfd8719c
William Santana <williamandrade1058@gmail.com>
2023-07-10 11:44:27 -0300

    update testes

--- secao5/estruturas_controle/testes.js
+++ secao5/estruturas_controle/testes.js
@@ -34,3 +34,16 @@ const pessoa = {
     }
 }
 console.log(pessoa.endereco.rua)
+
+
+let contador = 1
+
+while (contador <= 10){
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+for (let i = 0; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
