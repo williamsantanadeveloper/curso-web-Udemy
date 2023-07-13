b4282c16cec9d40543975a4a4e8719c95d588bf7
William Santana <williamandrade1058@gmail.com>
2023-07-13 11:33:10 -0300

    update testes

--- secao6/funcao/testes.js
+++ secao6/funcao/testes.js
@@ -1,4 +1,25 @@
-const soma = (a, b) => {
-    return a + b
+// const soma = (a, b) => {
+//     return a + b
+// }
+// console.log(soma(2, 4))
+
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar() {
+        console.log(this.saudacao)
+    }
 }
-console.log(soma(2, 4))
+const falar_saudacao = pessoa.falar.bind(pessoa)
+falar_saudacao()
+
+function pessoa2() {
+    this.idade = 0
+
+    const self = this
+
+    setInterval(function () {
+        self.idade++
+        console.log(self.idade)
+    }.bind(this))
+}
+new pessoa2
