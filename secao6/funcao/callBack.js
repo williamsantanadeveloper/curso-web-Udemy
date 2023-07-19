8fb536a4a79c5c05b7b4c792bc9002a0f4383972
William Santana <williamandrade1058@gmail.com>
2023-07-19 17:08:47 -0300

    testando os códigos

--- secao6/funcao/callBack.js
+++ secao6/funcao/callBack.js
@@ -1,6 +1,6 @@
 // Call Back: a ideia do call back é você passar uma função para outra função e como um determinado evento acontecer essa funcção que você passou vai ser disparada vai ser chamada de volta
 
-//forEach: Trata-se de um recurso muito útil para ser utilizado sempre que queremos que uma determinada função seja aplicada em todos os elementos de um array, pois elimina a necessidade de escrevermos muitas linhas de códigos
+//forEach: O método forEach em JavaScript é uma estrutura de repetição que permite executar uma função para cada item de um array. Ele é uma alternativa ao loop for, e é mais limpo, legível e fácil de manter.
 
 const fabricantes = ['Mercedes', 'Audi', 'BMW']
 
--- secao6/funcao/testes.js
+++ secao6/funcao/testes.js
@@ -1,25 +1,41 @@
-const soma = (a, b) => {
-    return a + b
-}
-console.log(soma(2, 4))
+// const soma = (a, b) => {
+//     return a + b
+// }
+// console.log(soma(2, 4))
 
-const pessoa = {
-    saudacao: 'Bom dia!',
-    falar() {
-        console.log(this.saudacao)
-    }
-}
-const falar_saudacao = pessoa.falar.bind(pessoa)
-falar_saudacao()
+// const pessoa = {
+//     saudacao: 'Bom dia!',
+//     falar() {
+//         console.log(this.saudacao)
+//     }
+// }
+// const falar_saudacao = pessoa.falar.bind(pessoa)
+// falar_saudacao()
 
-function pessoa2() {
-    this.idade = 0
+// function pessoa2() {
+//     this.idade = 0
 
-    const self = this
+//     const self = this
 
-    setInterval(function () {
-        self.idade++
-        console.log(self.idade)
-    }.bind(this))
-}
-new pessoa2
+//     setInterval(function () {
+//         self.idade++
+//         console.log(self.idade)
+//     }.bind(this))
+// }
+// new pessoa
+
+// imprimirAreaCirculo = (a,b) => {
+//     a = Math.PI
+//     b = Math.pow(2,3)
+//     let areaCirculo = a * b
+//     console.log(areaCirculo)
+
+//     this.getNewAreaCirculo(function(){
+//         return
+//     })
+// }
+// imprimirAreaCirculo()
+
+let numeros = [1,2,3,4,5,6]
+const maiorNumero = Math.max(numeros)
+console.log(maiorNumero)
