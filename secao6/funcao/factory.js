7c7f040d9ab98fcef09f501ca68197a81782d7ec
William Santana <williamandrade1058@gmail.com>
2023-07-22 18:21:24 -0300

    update factory

---
+++ secao6/funcao/factory.js
@@ -0,0 +1,10 @@
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+console.log(criarPessoa())
+
+// Função factory é uma função que retorna um objeto
