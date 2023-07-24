d3283227500a5f4a605eb378354064d65a2d5b52
William Santana <williamandrade1058@gmail.com>
2023-07-24 14:36:27 -0300

    update desafioConstrutora

--- secao6/funcao/desafioConstrutora.js
+++ secao6/funcao/desafioConstrutora.js
@@ -9,3 +9,14 @@ class Pessoa {
 }
 const p1 = new Pessoa('João')
 p1.falar()
+
+console.log('------------desafio resolvido abaixo------------------')
+function criarPessoa(nome) {
+    this.nome = nome
+
+    this.falar = () => {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+const p2 = new criarPessoa('William')
+p2.falar()
