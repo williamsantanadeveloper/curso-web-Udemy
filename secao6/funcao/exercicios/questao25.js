9e39e72d623abb1b3e65fafc2e26f75f9572fbfe
William Santana <williamandrade1058@gmail.com>
2023-07-31 16:33:07 -0300

    resolução questão 25

---
+++ secao6/funcao/exercicios/questao25.js
@@ -0,0 +1,13 @@
+// Resolução com for
+for (let i = 1; i <= 50; i++) {
+    console.log(`i = ${i}`)
+}
+
+console.log('---------------------------')
+
+// Resolução com while
+let contador = 0
+while (contador <= 50) {
+    console.log(`contador = ${contador}`)
+    contador++
+}
