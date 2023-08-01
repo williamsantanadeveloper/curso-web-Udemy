a05307d65709698bc563e452f2ca5a9224a4df8f
William Santana <williamandrade1058@gmail.com>
2023-08-01 10:19:10 -0300

    resolução questão 18

---
+++ secao6/funcao/exercicios/questao18.js
@@ -0,0 +1,42 @@
+por_extenso = (numero) => {
+    switch (Math.floor(numero)) {
+
+        case 10: console.log('Dez')
+            break
+        case 9: console.log('Nove')
+            break
+        case 8: console.log('Oito')
+            break
+        case 7: console.log('Sete')
+            break
+        case 6: console.log('Seis')
+            break
+        case 5: console.log('Cinco')
+            break
+        case 4: console.log('Quatro')
+            break
+        case 3: console.log('Três')
+            break
+        case 2: console.log('Dois')
+            break
+        case 1: console.log('Um')
+            break
+        case 0: console.log('Zero')
+            break
+        default: console.log('Número fora do intervalo')
+
+    }
+}
+
+por_extenso(10)
+por_extenso(9)
+por_extenso(8)
+por_extenso(7)
+por_extenso(6)
+por_extenso(5)
+por_extenso(4)
+por_extenso(3)
+por_extenso(2)
+por_extenso(1)
+por_extenso(0)
+por_extenso(11)
