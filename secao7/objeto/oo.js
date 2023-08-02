04c74ff63c0c5b33717074370e89a908de5f28b5
William Santana <williamandrade1058@gmail.com>
2023-08-02 20:21:46 -0300

    atualizado arquivo oo

---
+++ secao7/objeto/oo.js
@@ -0,0 +1,21 @@
+// Código não executável!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+
+    }
+}
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1ª Abstração: verificar se aquele código faz sentido para a empresa/local que você está trabalhando. Entender o mundo real e transformar em um software é um desafio da orientação a objetos
+// 2º Encapsulamento: ter os detalhes de implementação escondidos e você mostrar para quem precisa usar aquele sistema
+// 3º Herança (prototype): é um princípio baseado numa relação é um e existe algumas formas dentro do mundo do software de você reusar código 
+// 4º polimorfismo: significa múltiplas formas 
