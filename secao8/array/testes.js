aaad1a1ddfabcd331c61748bcf565da8e8779818
William Santana <williamandrade1058@gmail.com>
2023-08-31 11:58:05 -0300

    update

--- secao8/array/testes.js
+++ secao8/array/testes.js
@@ -2,3 +2,16 @@ nomes = (nome) => {
     return { nome }
 }
 console.log(nomes(1, 'William'))
+
+function product(name, price) {
+    this.name = name
+    this.price = price
+    this.desc = 0.15
+
+    this.get_desc = () => {
+        console.log(`${this.name} custa ${this.price}, mas com o desconto fica ${(this.price - (this.desc * this.price))}`)
+    }
+}
+
+let value_product = new product('Notebook', 1000)
+value_product.get_desc()
