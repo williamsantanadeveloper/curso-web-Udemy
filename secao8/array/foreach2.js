424bbdc31d6f0194db31971ffe8a888107a8a32c
William Santana <williamandrade1058@gmail.com>
2023-09-09 16:40:10 -0300

    adicionando arquivo foreach2.js

---
+++ secao8/array/foreach2.js
@@ -0,0 +1,13 @@
+Array.prototype.forEach2 = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+
+
+const approved = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+approved.forEach2(function (name, index, array) {
+    console.log(`${index + 1}) ${name}`)
+    console.log(array)
+})
