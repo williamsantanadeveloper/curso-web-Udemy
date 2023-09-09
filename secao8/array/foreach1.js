f1e9984a3dc03e5911f7475248f182bfac7f098f
William Santana <williamandrade1058@gmail.com>
2023-09-09 15:04:11 -0300

    arquivo foreach.js modificado

--- secao8/array/foreach1.js
+++ secao8/array/foreach1.js
@@ -1,14 +1,11 @@
 const approved = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
-approved.forEach(function (name, index, array) {
+approved.forEach(function (name, index) {
     console.log(`${index + 1}) ${name}`)
-    console.log(array)
 })
-
+// a função call back que passa para o forEach ela só pode receber 3 parâmetros(nome, indice, array)
 approved.forEach(name => console.log(name))
 
 const show_approved = approved => console.log(approved)
 console.log('----------')
 approved.forEach(show_approved)
-
-console.log('-----------')
