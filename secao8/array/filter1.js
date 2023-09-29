c5081eea0ffeb19ec22f63669c9d154b130a0183
William Santana <williamandrade1058@gmail.com>
2023-09-29 10:41:00 -0300

    add imperativo_vs_declarativo.js

--- secao8/array/filter1.js
+++ secao8/array/filter1.js
@@ -3,13 +3,13 @@ const products = [
     { name: 'Ipad Pro', price: 4199, fragile: true },
     { name: 'Glass', price: 12.49, fragile: true },
     { name: 'Plastic coup', price: 18.99, fragile: false }
-]
+];
 
 console.log(products.filter(function (p) {
     return false
-}))
+}));
 
-const higher_price = products => products.price >= 500
-const fragile_true = products => products.fragile
+const higher_price = products => products.price >= 500;
+const fragile_true = products => products.fragile;
 
-console.log(products.filter(higher_price).filter(fragile_true))
+console.log(products.filter(higher_price).filter(fragile_true));
