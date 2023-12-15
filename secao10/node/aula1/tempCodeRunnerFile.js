483e6e33e53b653bbed03b0c5e2a87dab5b34882
William Santana <williamandrade1058@gmail.com>
2023-12-15 12:58:41 -0300

    up

--- secao10/node/aula1/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-console.log(moduleA);
-// console.log(moduleA.welcome);
-// console.log(moduleA.hello);
-// console.log(moduleA.seeYouSoon);
-// console.log('------------------------');
-// console.log(moduleB.goodMorning);
-// console.log(moduleB.goodEvening());
---
+++ secao10/node/usingModulesThirdParty.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+const path = require.resolve('lodash/index');
+const _ = require('loadash');
+
+
+console.log(path)
+setInterval(() => console.log(_.random(1, 1000)), 2000);
